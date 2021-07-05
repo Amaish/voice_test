a25a5a47255cae57d7459fa0a4058d529c988457
Anthony <anthonymaina.nganga@gmail.com>
2021-07-05 11:03:47 +0300

    first push

---
+++ web/handlers.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"text/template"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+	ts, err := template.ParseFiles("./ui/html/home.html")
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+		return
+	}
+
+	err = ts.Execute(w, nil)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+	}
+}
+
+func dial(w http.ResponseWriter, r *http.Request) {
+	ts, err := template.ParseFiles("./ui/xml/dial.xml")
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+		return
+	}
+
+	err = ts.Execute(w, nil)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+	}
+}
+
+func play(w http.ResponseWriter, r *http.Request) {
+	ts, err := template.ParseFiles("./ui/xml/play.xml")
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+		return
+	}
+
+	err = ts.Execute(w, nil)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+	}
+}
+
+func say(w http.ResponseWriter, r *http.Request) {
+	ts, err := template.ParseFiles("./ui/xml/say.xml")
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+		return
+	}
+
+	err = ts.Execute(w, nil)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+	}
+}
+
+func reject(w http.ResponseWriter, r *http.Request) {
+	ts, err := template.ParseFiles("./ui/xml/reject.xml")
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+		return
+	}
+
+	err = ts.Execute(w, nil)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+	}
+
+}
